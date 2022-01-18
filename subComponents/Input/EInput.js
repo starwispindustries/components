71d836a02a75db75f68e836655b892d97677ff85
amishranpariya <amishranpariya@gmail.com>
2022-01-18 11:29:29 +0530

    updated the input file

---
+++ subComponents/Input/EInput.js
@@ -0,0 +1,264 @@
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import {
+	Box,
+	FormLabel,
+	Input,
+	InputGroup,
+	InputLeftElement,
+	InputRightElement,
+	Text,
+	Textarea,
+	useColorModeValue,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const EInput = ({
+	type,
+	inputMode,
+	name = type,
+	label = name,
+	value,
+	onChange,
+	placeholder = label,
+	leftIcon,
+	rightIcon,
+	isFullWidth,
+	error,
+	isInvalid,
+	isDisabled,
+	isReadOnly,
+	isRequired,
+	autoComplete,
+	maxCharacters,
+	...rest
+}) => {
+	const borderColor = useColorModeValue("#f0f0f0", "#2E2E2E");
+	const errorBorderColor = useColorModeValue("red.e200", "red.e200");
+	const focusBorderColor = useColorModeValue(
+		"primary.light.200",
+		"primary.dark.200"
+	);
+	const hoverBorderColor = useColorModeValue(
+		"primary.light.200",
+		"primary.dark.200"
+	);
+	const placeHolderColor = useColorModeValue(
+		"text.light.disabled",
+		"text.dark.disabled"
+	);
+	const textColor = useColorModeValue(
+		"text.light.h1_body",
+		"text.dark.h1_body"
+	);
+	const labelColor = useColorModeValue(
+		"text.light.h3_captions",
+		"text.dark.h3_captions"
+	);
+	const labelBGColor = useColorModeValue(
+		"backgrounds.light.e000",
+		"backgrounds.dark.e000"
+	);
+	const iconLeftColor = useColorModeValue(
+		"text.light.disabled",
+		"text.dark.disabled"
+	);
+	const iconRightColor = useColorModeValue(
+		"text.light.subtext",
+		"text.dark.subtext"
+	);
+
+	const [passwordVisible, setPasswordVisible] = useState(false);
+	let handleToggle = (e) => {
+		e.preventDefault();
+	};
+	let inputElement = null;
+	switch (type) {
+		case "text":
+			inputMode = inputMode || "text";
+			break;
+
+		case "password":
+			handleToggle = (e) => {
+				e.preventDefault();
+				setPasswordVisible((visibility) => !visibility);
+			};
+			rightIcon = passwordVisible ? <ViewOffIcon /> : <ViewIcon />;
+			break;
+
+		case "email":
+			inputMode = inputMode || "email";
+			break;
+
+		case "tel":
+			inputMode = inputMode || "tel";
+			break;
+
+		case "url":
+			inputMode = inputMode || "url";
+			break;
+
+		case "textarea":
+			break;
+		default:
+			inputElement = <Input name={name} {...rest} />;
+			break;
+	}
+
+	return (
+		<Box position={"relative"} mt={2}>
+			{label && (
+				<FormLabel
+					position={"absolute"}
+					top={"-8px"}
+					left={"12px"}
+					display={"block"}
+					fontSize={"12px"}
+					fontWeight={500}
+					color={labelColor}
+					borderRadius={"5px"}
+					mr={"auto"}
+					px={"7px"}
+					bg={labelBGColor}
+					zIndex={2}
+				>
+					{label}
+				</FormLabel>
+			)}
+			{type === "textarea" && maxCharacters && (
+				<FormLabel
+					position={"absolute"}
+					top={"-8px"}
+					right={"12px"}
+					display={"block"}
+					fontSize={"12px"}
+					fontWeight={500}
+					color={labelColor}
+					borderRadius={"5px"}
+					mr={"auto"}
+					px={"7px"}
+					bg={labelBGColor}
+					zIndex={2}
+				>
+					{maxCharacters}
+				</FormLabel>
+			)}
+			<InputGroup w={isFullWidth ? "full" : "302px"} maxW={"full"}>
+				{leftIcon && (
+					<InputLeftElement
+						pointerEvents="none"
+						color={iconLeftColor}
+						h={"52px"}
+						mt={"2px"}
+						ml={1}
+					>
+						{leftIcon}
+					</InputLeftElement>
+				)}
+
+				{type === "textarea" ? (
+					<Box
+						h={"266px"}
+						overflow={"hidden"}
+						w={isFullWidth ? "full" : "302px"}
+						border={"1.5px solid"}
+						borderColor={
+							isDisabled
+								? borderColor
+								: isInvalid
+								? errorBorderColor
+								: focusBorderColor
+						}
+						borderRadius={"10px"}
+						bg={"transparent"}
+					>
+						<Textarea
+							resize={"none"}
+							name={name}
+							w={isFullWidth ? "full" : "302px"}
+							h={"266px"}
+							maxW={"full"}
+							_placeholder={{
+								color: placeHolderColor,
+								fontSize: "inherit",
+								fontWeight: "inherit",
+							}}
+							autoComplete={autoComplete}
+							border={"0px solid"}
+							borderColor={"transparent"}
+							borderRadius={"10px"}
+							bg={"transparent"}
+							color={textColor}
+							fontSize={"16px"}
+							fontWeight={400}
+							inputMode={inputMode}
+							isDisabled={isDisabled}
+							isInvalid={isInvalid || error}
+							isRequired={isRequired}
+							placeholder={placeholder}
+							value={value}
+							onChange={onChange}
+							{...rest}
+						></Textarea>
+					</Box>
+				) : (
+					<Input
+						name={name}
+						type={
+							type !== "password" ? type : passwordVisible ? "text" : "password"
+						}
+						w={isFullWidth ? "full" : "302px"}
+						maxW={"full"}
+						h={"52px"}
+						_focus={{ borderColor: focusBorderColor, borderWidth: "1.5px" }}
+						_hover={{ borderColor: hoverBorderColor }}
+						_invalid={{ borderColor: errorBorderColor, borderWidth: "1.5px" }}
+						_placeholder={{
+							color: placeHolderColor,
+							fontSize: "inherit",
+							fontWeight: "inherit",
+						}}
+						_disabled={{ borderColor: borderColor }}
+						autoComplete={autoComplete}
+						border={"1.5px solid"}
+						borderColor={focusBorderColor}
+						borderRadius={"10px"}
+						bg={"transparent"}
+						color={textColor}
+						fontSize={"16px"}
+						fontWeight={400}
+						inputMode={inputMode}
+						isDisabled={isDisabled}
+						isInvalid={isInvalid || error}
+						isRequired={isRequired}
+						placeholder={placeholder}
+						value={value}
+						onChange={onChange}
+						{...rest}
+					/>
+				)}
+				{rightIcon && (
+					<InputRightElement
+						h={"52px"}
+						color={iconRightColor}
+						onClick={handleToggle}
+					>
+						{rightIcon}
+					</InputRightElement>
+				)}
+			</InputGroup>
+			{error && (
+				<Text
+					mt={"10px"}
+					fontSize={"14px"}
+					lineHeight={"17px"}
+					fontWeight={"300"}
+					color={errorBorderColor}
+				>
+					{error}
+				</Text>
+			)}
+		</Box>
+	);
+};
+
+export default EInput;
