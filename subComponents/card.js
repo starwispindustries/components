d94b51ed1d879ade925ffb88db70853a156d3fb1
Ritik-Sharma38 <rd@ritiksharma.in>
2022-01-19 13:58:39 +0530

    updated

--- subComponents/card.js
+++ subComponents/card.js
@@ -1,5 +1,6 @@
 import { useColorModeValue, Box } from '@chakra-ui/react'
 import { mode, whiten, darken } from '@chakra-ui/theme-tools'
+
 export const CardSecondary = (props) => {
     const card_bg = useColorModeValue('backgrounds.light.e100', 'backgrounds.dark.e100')
     const hover_bg = useColorModeValue(darken('backgrounds.light.e100', 3), whiten('backgrounds.dark.e100', 15))
