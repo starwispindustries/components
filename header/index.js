3eeb9aef02ce957e7ec7d560f5c77ff41381517a
amishranpariya <amishranpariya@gmail.com>
2022-01-20 08:59:20 +0530

    header back button edited, bug fixed

--- header/index.js
+++ header/index.js
@@ -1,26 +1,37 @@
-import { Flex, Icon, useColorModeValue } from "@chakra-ui/react"
-import { ChevronLeftIcon } from '@chakra-ui/icons'
+import { Flex, IconButton, useColorModeValue } from "@chakra-ui/react";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
 
-const Header = (props) => {
-    console.log(props)
-    const arrowColor = useColorModeValue("primary.light.200", "primary.dark.200")
-    const bg = useColorModeValue("primary.light._000", "primary.dark._000")
-    return (
-        <Flex
-            transition="0.5s opacity"
-            as="nav"
-            h="63px"
-            p="0 15px"
-            bg={bg}
-            width="full"
-            align="center"
-            {...props}
-        >
-            
-            <ChevronLeftIcon w="32px" h="32px" p={1} color={arrowColor} onClick={props.goback} focusable={true} borderRadius="50%"/>
-            {props.children}
-        </Flex>
-    )
-}
+const Header = ({ goback, children, ...props }) => {
+	const arrowColor = useColorModeValue("primary.light.200", "primary.dark.200");
+	const bg = useColorModeValue("primary.light._000", "primary.dark._000");
+	return (
+		<Flex
+			transition="0.5s opacity"
+			as="nav"
+			h="63px"
+			p="0 15px"
+			bg={bg}
+			width="full"
+			align="center"
+			{...props}
+		>
+			{goback && (
+				<IconButton
+					variant={"transparent"}
+					w="28px"
+					h="28px"
+					_focus={{ outline: "none" }}
+					bg={"transparent"}
+					borderRadius={"full"}
+					color={arrowColor}
+					onClick={(e) => goback(e)}
+				>
+					<ChevronLeftIcon w={"28px"} h={"28px"} />
+				</IconButton>
+			)}
+			{children}
+		</Flex>
+	);
+};
 
 export default Header;
