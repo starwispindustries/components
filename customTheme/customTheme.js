5428edb41e2f506165643bff8b51cacee16665c7
amishranpariya <amishranpariya@gmail.com>
2022-01-27 12:22:19 +0530

    secondary button border width updated

--- customTheme/customTheme.js
+++ customTheme/customTheme.js
@@ -215,7 +215,7 @@ const theme = extendTheme({
 				}),
 				secondary: (props) => ({
 					bg: "transparent",
-					border: "1px solid",
+					border: "2px solid",
 					borderColor: mode("primary.light.200", "primary.dark.200")(props),
 					color: mode("primary.light.200", "primary.dark.200")(props),
 					_hover: {
