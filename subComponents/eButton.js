d5c641517e590871948c302a55320794c5a35175
amishranpariya <amishranpariya@gmail.com>
2022-02-28 18:00:55 +0530

    button focus removed

--- subComponents/eButton.js
+++ subComponents/eButton.js
@@ -1,5 +1,9 @@
 import { Button } from "@chakra-ui/react";
 
 export function EButton(props) {
-	return <Button {...props}>{props.children}</Button>;
+	return (
+		<Button _focus={{ outline: "none" }} {...props}>
+			{props.children}
+		</Button>
+	);
 }
