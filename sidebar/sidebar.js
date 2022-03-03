3065a5657b48a04d4f96ec3e6f4bb273b7a5eafc
amishranpariya <amishranpariya@gmail.com>
2022-03-03 17:04:25 +0530

    5431 fixed

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -26,11 +26,12 @@ const CustomIcon = ({ children }) => {
 	);
 };
 
-const CustomButton = ({ children, path, active }) => {
+const CustomButton = ({ children, path, active, isDark }) => {
 	return (
 		<Link href={path}>
 			<Button
 				w="100%"
+				key={"" + isDark}
 				variant={active ? "active_sidebar_button" : "sidebar_button"}
 				size="sm"
 				_focus={{ outline: "none" }}
@@ -41,7 +42,7 @@ const CustomButton = ({ children, path, active }) => {
 	);
 };
 
-const SidebarContent = ({}) => {
+const SidebarContent = ({ isDark }) => {
 	const router = useRouter();
 	const origin =
 		typeof window !== "undefined" && window.location.origin
@@ -53,6 +54,7 @@ const SidebarContent = ({}) => {
 			<CustomButton
 				path={"https://main.edvora.me/"}
 				active={origin.includes("main.edvora.me") && router.pathname === "/"}
+				isDark={isDark}
 			>
 				<CustomIcon>
 					<path d="M10.8333 2.5V7.5H17.5V2.5H10.8333ZM10.8333 17.5H17.5V9.16667H10.8333V17.5ZM2.5 17.5H9.16667V12.5H2.5V17.5ZM2.5 10.8333H9.16667V2.5H2.5V10.8333Z" />
@@ -62,6 +64,7 @@ const SidebarContent = ({}) => {
 			<CustomButton
 				path={"https://classrooms.edvora.me/"}
 				active={origin.includes("classrooms.edvora.me")}
+				isDark={isDark}
 			>
 				<CustomIcon>
 					<path d="M10 5.6886C10.7735 5.6886 11.5154 5.99589 12.0624 6.54287C12.6094 7.08985 12.9167 7.83172 12.9167 8.60527C12.9167 9.37881 12.6094 10.1207 12.0624 10.6677C11.5154 11.2146 10.7735 11.5219 10 11.5219C9.22645 11.5219 8.48459 11.2146 7.93761 10.6677C7.39062 10.1207 7.08333 9.37881 7.08333 8.60527C7.08333 7.83172 7.39062 7.08985 7.93761 6.54287C8.48459 5.99589 9.22645 5.6886 10 5.6886ZM4.16667 7.77193C4.63333 7.77193 5.06667 7.89693 5.44167 8.12193C5.31667 9.3136 5.66667 10.4969 6.38333 11.4219C5.96667 12.2219 5.13333 12.7719 4.16667 12.7719C3.50363 12.7719 2.86774 12.5085 2.3989 12.0397C1.93006 11.5709 1.66667 10.935 1.66667 10.2719C1.66667 9.60889 1.93006 8.97301 2.3989 8.50416C2.86774 8.03532 3.50363 7.77193 4.16667 7.77193ZM15.8333 7.77193C16.4964 7.77193 17.1323 8.03532 17.6011 8.50416C18.0699 8.97301 18.3333 9.60889 18.3333 10.2719C18.3333 10.935 18.0699 11.5709 17.6011 12.0397C17.1323 12.5085 16.4964 12.7719 15.8333 12.7719C14.8667 12.7719 14.0333 12.2219 13.6167 11.4219C14.3333 10.4969 14.6833 9.3136 14.5583 8.12193C14.9333 7.89693 15.3667 7.77193 15.8333 7.77193ZM4.58333 16.3136C4.58333 14.5886 7.00833 13.1886 10 13.1886C12.9917 13.1886 15.4167 14.5886 15.4167 16.3136V17.7719H4.58333V16.3136ZM0 17.7719V16.5219C0 15.3636 1.575 14.3886 3.70833 14.1053C3.21667 14.6719 2.91667 15.4553 2.91667 16.3136V17.7719H0ZM20 17.7719H17.0833V16.3136C17.0833 15.4553 16.7833 14.6719 16.2917 14.1053C18.425 14.3886 20 15.3636 20 16.5219V17.7719Z" />
@@ -72,6 +75,7 @@ const SidebarContent = ({}) => {
 			<CustomButton
 				path={"https://main.edvora.me/i"}
 				active={origin.includes("main.edvora.me") && router.pathname === "/i"}
+				isDark={isDark}
 			>
 				<CustomIcon>
 					<g clipPath="url(#clip0_20_146)">
@@ -89,7 +93,7 @@ const SidebarContent = ({}) => {
 	);
 };
 
-const Sidebar = ({ variant }) => {
+const Sidebar = ({ variant, isDark }) => {
 	const bg = useColorModeValue(
 		"backgrounds.light.e000",
 		"backgrounds.dark.e000"
@@ -115,7 +119,7 @@ const Sidebar = ({ variant }) => {
 				<Image src="/ed_logo.png" alt="" width="21px" height="21px" />
 			</Center>
 			<Divider borderColor={borderColor} />
-			<SidebarContent />
+			<SidebarContent isDark={isDark} />
 		</Box>
 	) : (
 		<Drawer isOpen={isOpen} placement="left" onClose={onClose}>
@@ -124,7 +128,7 @@ const Sidebar = ({ variant }) => {
 					<DrawerCloseButton />
 					<DrawerHeader>Chakra-UI</DrawerHeader>
 					<DrawerBody>
-						<SidebarContent onClick={onClose} />
+						<SidebarContent onClick={onClose} isDark={isDark} />
 					</DrawerBody>
 				</DrawerContent>
 			</DrawerOverlay>
