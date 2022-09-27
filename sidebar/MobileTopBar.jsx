4aaba10269a06888e8b068843c8f9215da7069dc
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2022-09-27 15:33:48 +0530

    Adds notification modal - mobile

--- sidebar/MobileTopBar.jsx
+++ sidebar/MobileTopBar.jsx
@@ -5,8 +5,10 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 import React from "react";
+import { useState } from "react";
 import HamburgerMenu from "../Icons/HamburgerMenu";
 import NotificationIcon from "../Icons/NotificationIcon";
+import NotificationPopup from "../subComponents/Notifications/NotificationPopup";
 import UserAvatar from "../subComponents/UserAvatar";
 
 const MobileTopBar = ({ onOpen }) => {
@@ -15,35 +17,42 @@ const MobileTopBar = ({ onOpen }) => {
     "backgrounds.dark.e000"
   );
 
+  const [showNoti, setShowNoti] = useState(false);
+
   return (
-    <HStack w="full" p="15px" bg={bg}>
-      <IconButton
-        icon={<HamburgerMenu />}
-        onClick={onOpen}
-        w="30px"
-        h="30px"
-        bg="transparent"
-        _hover={{ bg: "transparent" }}
-      />
+    <>
+      {showNoti && <NotificationPopup />}
 
-      <Spacer />
-
-      <HStack spacing={"10px"}>
+      <HStack w="full" p="15px" bg={bg}>
         <IconButton
-          icon={<NotificationIcon />}
+          icon={<HamburgerMenu />}
+          onClick={onOpen}
           w="30px"
           h="30px"
           bg="transparent"
           _hover={{ bg: "transparent" }}
         />
 
-        <UserAvatar
-          fullName={"Ciddarth Raaj"}
-          filekey="f474760d-4ee5-4f14-b93f-8871ab5735db"
-          size="xs"
-        />
+        <Spacer />
+
+        <HStack spacing={"10px"}>
+          <IconButton
+            icon={<NotificationIcon />}
+            w="30px"
+            h="30px"
+            bg="transparent"
+            _hover={{ bg: "transparent" }}
+            onClick={() => setShowNoti(!showNoti)}
+          />
+
+          <UserAvatar
+            fullName={"Ciddarth Raaj"}
+            filekey="f474760d-4ee5-4f14-b93f-8871ab5735db"
+            size="xs"
+          />
+        </HStack>
       </HStack>
-    </HStack>
+    </>
   );
 };
 
