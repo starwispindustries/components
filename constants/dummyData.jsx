ce8c694c339d0a3337e8f9ca0c3fc379005d3c44
Ciddarth Raaj <ciddarthjeyakumar@gmail.com>
2022-09-28 13:20:24 +0530

    Removes dummy data

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,9 +1,9 @@
-export const dummyUser = {
-    token: "11e99e32-7918-4725-99f9-112f6be55434",
-    username: "cs",
-    organization_id: "101",
-    session_id: "85079848-f044-4d86-8105-4e059abfffeb",
-    role_id: "6201106aaadbcc1442b4b15a",
-};
+// export const dummyUser = {
+//     token: "11e99e32-7918-4725-99f9-112f6be55434",
+//     username: "cs",
+//     organization_id: "101",
+//     session_id: "85079848-f044-4d86-8105-4e059abfffeb",
+//     role_id: "6201106aaadbcc1442b4b15a",
+// };
 
-// export const dummyUser = undefined;
+export const dummyUser = undefined;
