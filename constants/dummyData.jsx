3b1580800f843526dd76f415c7db6edfb4999df6
Ritik-Sharma38 <rd@ritiksharma.in>
2022-10-24 15:44:26 +0530

    fix: udpates

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -5,14 +5,14 @@
 //   session_id: "7f567681-ee10-4f63-9fb2-5916b1ad658e",
 //   role_id: "62011052aadbcc1442b4b159",
 // };
-export const dummyUser = {
-    "token": "dd7fc705-d61a-4243-b5af-20a493a5f42c",
-    "username": "rtu",
-    "when_accepted_terms": 0,
-    "organization_id": "101",
-    "session_id": "f17aa242-3f8d-450d-9693-50d666526d24",
-    "role_id": "62011052aadbcc1442b4b159",
-    "theme": 0
-}
+// export const dummyUser = {
+//     "token": "dd7fc705-d61a-4243-b5af-20a493a5f42c",
+//     "username": "rtu",
+//     "when_accepted_terms": 0,
+//     "organization_id": "101",
+//     "session_id": "f17aa242-3f8d-450d-9693-50d666526d24",
+//     "role_id": "62011052aadbcc1442b4b159",
+//     "theme": 0
+// }
 
-// export const dummyUser = undefined;
+export const dummyUser = undefined;
