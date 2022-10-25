ef748d2e4efba604488f4534f91c814d6f54f6f5
Ritik-Sharma38 <rd@ritiksharma.in>
2022-10-25 11:40:56 +0530

    fix: data

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,11 +1,11 @@
-export const dummyUser = {
-    "token": "65965279-9e2d-47b2-ac73-b7437233e4b2",
-    "username": "rtu",
-    "when_accepted_terms": 1658952663,
-    "organization_id": "101",
-    "session_id": "754ebeec-635d-4acc-b9d6-c62e934a250b",
-    "role_id": "62011052aadbcc1442b4b159",
-    "theme": 0
-}
+// export const dummyUser = {
+//     "token": "65965279-9e2d-47b2-ac73-b7437233e4b2",
+//     "username": "rtu",
+//     "when_accepted_terms": 1658952663,
+//     "organization_id": "101",
+//     "session_id": "754ebeec-635d-4acc-b9d6-c62e934a250b",
+//     "role_id": "62011052aadbcc1442b4b159",
+//     "theme": 0
+// }
 
-// export const dummyUser = undefined;
+export const dummyUser = undefined;
