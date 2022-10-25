03376fbf90597386ac545706670113e01300bd25
Ritik-Sharma38 <rd@ritiksharma.in>
2022-10-25 15:24:34 +0530

    fix: data

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -6,11 +6,11 @@
 //   role_id: "62011052aadbcc1442b4b159",
 // };
 // export const dummyUser = {
-//     token: "11e99e32-7918-4725-99f9-112f6be55434",
-//     username: "cs",
+//     token: "2ac1072b-4505-48a9-801e-8a2add03b1a4",
+//     username: "rd0",
 //     organization_id: "101",
-//     session_id: "85079848-f044-4d86-8105-4e059abfffeb",
-//     role_id: "6201106aaadbcc1442b4b15a",
+//     session_id: "324d0a74-5479-4909-85ef-d1f43d89b527",
+//     role_id: "62011052aadbcc1442b4b159",
 // };
 
 export const dummyUser = undefined;
