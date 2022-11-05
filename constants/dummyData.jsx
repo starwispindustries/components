3a8f8aaf3b3e33ed44606f04467f591f977dfffd
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-05 12:26:59 +0530

    update: comp

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,9 +1,9 @@
 // export const dummyUser = {
-//     "token": "52d1d333-bafe-4699-9a0c-a86408af9752",
-//     "username": "Ritik_Sharma",
-//     "when_accepted_terms": 1667189686,
+//     "token": "2207aecb-d06f-47d9-8e9a-5f4697c034ed",
+//     "username": "chris_evans",
+//     "when_accepted_terms": 1667190528,
 //     "organization_id": "101",
-//     "session_id": "19cbc771-7a85-4b9a-b513-61b2b29f8924",
+//     "session_id": "6cc4eb6f-ecb2-441c-91b2-82386729dde6",
 //     "role_id": "62011052aadbcc1442b4b159",
 //     "theme": 2
 // }
