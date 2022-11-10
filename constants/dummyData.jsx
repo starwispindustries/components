40daa769341500ed779753e2588d58064afe8d9b
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-10 10:46:00 +0530

    fix: readCookie

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,9 +1,9 @@
 // export const dummyUser = {
-//     "token": "533864ad-a965-4ef7-90b8-9e967ff7edc3",
+//     "token": "b186ae4b-75cc-47a9-b713-516b53c9817c",
 //     "username": "Ritik_Sharma",
 //     "when_accepted_terms": 1667189686,
 //     "organization_id": "101",
-//     "session_id": "b0ddc7e1-5997-4cd8-8829-080c7e3952aa",
+//     "session_id": "f84356b9-0acf-4b9a-a6f4-221aa2e34640",
 //     "role_id": "62011052aadbcc1442b4b159",
 //     "theme": 2
 // }
