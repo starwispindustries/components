9994015a00f94203dc233d7a79d34184755880aa
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-10 12:05:38 +0530

    fix: cmp

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,10 +1,10 @@
 // export const dummyUser = {
-//     "token": "b186ae4b-75cc-47a9-b713-516b53c9817c",
-//     "username": "Ritik_Sharma",
-//     "when_accepted_terms": 1667189686,
+//     "token": "6830bb1c-142d-41fd-a00b-c9b6fdb9f8d1",
+//     "username": "mark_evans",
+//     "when_accepted_terms": 1667190694,
 //     "organization_id": "101",
-//     "session_id": "f84356b9-0acf-4b9a-a6f4-221aa2e34640",
-//     "role_id": "62011052aadbcc1442b4b159",
-//     "theme": 2
+//     "session_id": "22961628-47ef-499c-af99-ae22499c991f",
+//     "role_id": "6201106aaadbcc1442b4b15a",
+//     "theme": 0
 // }
-// export const dummyUser = undefined;
+export const dummyUser = undefined;
