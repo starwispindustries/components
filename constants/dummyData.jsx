a4b7f407428e2989fbe2b89ce828e62213cb3964
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-14 15:30:42 +0530

    fix: sidebar

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,10 +1,10 @@
 // export const dummyUser = {
-//     "token": "0878cefa-f613-466a-b9cb-d21b69be9271",
-//     "username": "usertest0",
-//     "when_accepted_terms": 1668074882,
+//     "token": "f82401cc-54de-4eb7-ada4-cb8a8a2fd249",
+//     "username": "Ritik_Sharma",
+//     "when_accepted_terms": 1667189686,
 //     "organization_id": "101",
-//     "session_id": "9db53855-941f-48d0-8490-b4abb73b273a",
-//     "role_id": "6201106aaadbcc1442b4b15a",
-//     "theme": 0
+//     "session_id": "af986d00-fa7c-419e-a5db-839386be624a",
+//     "role_id": "62011052aadbcc1442b4b159",
+//     "theme": 1
 // }
 export const dummyUser = undefined;
--- constants/index.jsx
+++ constants/index.jsx
@@ -1,30 +1,32 @@
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig()
 
 //ENV VARIABLES ================================================================================================================
 export const CLASSROOM_API_URL =
-  process.env.REACT_APP_CLASSROOM_API_URL ||
+  publicRuntimeConfig.REACT_APP_CLASSROOM_API_URL ||
   "https://classrooms.api.edvora.me/";
 export const TIMELINE_API_URL =
-  process.env.REACT_APP_TIMELINE_API_URL || "https://timetable.api.edvora.me/";
+  publicRuntimeConfig.REACT_APP_TIMELINE_API_URL || "https://timetable.api.edvora.me/";
 export const MAIN_API_URL =
-  process.env.REACT_APP_MAIN_API_URL || "https://main.api.edvora.me/";
+  publicRuntimeConfig.REACT_APP_MAIN_API_URL || "https://main.api.edvora.me/";
 export const AUTH_API_URL =
-  process.env.REACT_APP_AUTH_API_URL || "https://auth.api.edvora.me/";
+  publicRuntimeConfig.REACT_APP_AUTH_API_URL || "https://auth.api.edvora.me/";
 export const FILE_API_URL =
-  process.env.REACT_APP_FILE_API_URL || "https://files.api.edvora.me/";
+  publicRuntimeConfig.REACT_APP_FILE_API_URL || "https://files.api.edvora.me/";
 
 export const MAIN_URL =
-  process.env.REACT_APP_MAIN_URL || "https://main.edvora.me/";
+  publicRuntimeConfig.REACT_APP_MAIN_URL || "https://main.edvora.me/";
 export const CLASSROOM_URL =
-  process.env.REACT_APP_CLASS_URL || "https://classrooms.edvora.me/";
+  publicRuntimeConfig.REACT_APP_CLASS_URL || "https://classrooms.edvora.me/";
 export const TIMELINE_URL =
-  process.env.REACT_APP_TIMELINE_URL || "https://timeline.edvora.me/";
+  publicRuntimeConfig.REACT_APP_TIMELINE_URL || "https://timeline.edvora.me/";
 export const LECTURES_URL =
-  process.env.REACT_APP_LECTURES_URL || "https://lectures.edvora.me/";
+  publicRuntimeConfig.REACT_APP_LECTURES_URL || "https://lectures.edvora.me/";
 
 export const AGORA_APP_ID =
-  process.env.REACT_APP_AGORA_APP_ID || "d7317e76e2374323823ada6aabcd8640";
+  publicRuntimeConfig.REACT_APP_AGORA_APP_ID || "d7317e76e2374323823ada6aabcd8640";
 
-  export const EDVORA_URL = process.env.REACT_APP_EDVORA_URL || 'https://www.edvora.com';
+  export const EDVORA_URL = publicRuntimeConfig.REACT_APP_EDVORA_URL || 'https://www.edvora.com';
 
 // ================================================================================================================================
 
