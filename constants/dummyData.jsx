67bf401e8debe9316647efd29b748b9833989f88
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-15 02:59:06 +0530

    fix: fix

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,10 +1,10 @@
 // export const dummyUser = {
-//     "token": "6830bb1c-142d-41fd-a00b-c9b6fdb9f8d1",
-//     "username": "mark_evans",
-//     "when_accepted_terms": 1667190694,
+//     "token": "e117b04e-34f9-4eb2-bb69-34cec21668c8",
+//     "username": "rj_f",
+//     "when_accepted_terms": 991562412,
 //     "organization_id": "101",
-//     "session_id": "22961628-47ef-499c-af99-ae22499c991f",
-//     "role_id": "6201106aaadbcc1442b4b15a",
+//     "session_id": "13b5c810-416a-4169-acbe-15ffad2c8444",
+//     "role_id": "62011052aadbcc1442b4b159",
 //     "theme": 0
 // }
 export const dummyUser = undefined;
