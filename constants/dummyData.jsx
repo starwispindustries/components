545562d5dc4845be73606798fbba32ccfd7e7485
Rahul Jha <rahulj.rj642@gmail.com>
2022-11-15 03:03:49 +0530

    fix: update

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,10 +1,10 @@
 // export const dummyUser = {
-//     "token": "e117b04e-34f9-4eb2-bb69-34cec21668c8",
-//     "username": "rj_f",
-//     "when_accepted_terms": 991562412,
+//     "token": "d60b6196-22dc-4977-8034-8f4a034e5a54",
+//     "username": "rj_a",
+//     "when_accepted_terms": 1668265016,
 //     "organization_id": "101",
-//     "session_id": "13b5c810-416a-4169-acbe-15ffad2c8444",
+//     "session_id": "50c68be5-44e3-41cc-8306-3fc39ee1b66f",
 //     "role_id": "62011052aadbcc1442b4b159",
 //     "theme": 0
-// }   
-export const dummyUser = undefined;
+// }  
+export const dummyUser = undefined;  
