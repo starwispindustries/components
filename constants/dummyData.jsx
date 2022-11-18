6a855c00aa2c61324b99273f820bcca625c0fd76
Rahul Jha <rahulj.rj642@gmail.com>
2022-11-18 16:09:45 +0530

    fix: dummy uncomment

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,10 +1,10 @@
 // export const dummyUser = {
-//     "token": "d60b6196-22dc-4977-8034-8f4a034e5a54",
+//     "token": "f763d0fa-7f06-4c65-a2fe-2e27c24b8c4f",
 //     "username": "rj_a",
 //     "when_accepted_terms": 1668265016,
 //     "organization_id": "101",
-//     "session_id": "50c68be5-44e3-41cc-8306-3fc39ee1b66f",
+//     "session_id": "6b9f7571-adcc-43a4-b6b7-6ba92c5d5081",
 //     "role_id": "62011052aadbcc1442b4b159",
 //     "theme": 0
 // }  
-export const dummyUser = undefined;  
+export const dummyUser = undefined;
