afd5ca57192b4a01b7a42bfefe46080ee68a263f
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-19 11:51:34 +0530

    fix: notification redirect

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,10 +1,10 @@
-// export const dummyUser = {
-//     "token": "97ef552f-dbd1-41d4-82fa-671b8ac39425",
-//     "username": "rj_z",
-//     "when_accepted_terms": 991562412,
-//     "organization_id": "101",
-//     "session_id": "c9506747-d550-4e0c-a70d-47204a9c0b77",
-//     "role_id": "6201106aaadbcc1442b4b15a",
-//     "theme": 1
-// }
-export const dummyUser = undefined;  
+export const dummyUser = {
+    "token": "bd259a03-6520-4317-ad43-b29c7372424a",
+    "username": "rj_c",
+    "when_accepted_terms": 991562412,
+    "organization_id": "101",
+    "session_id": "66074e3f-6f25-4381-b747-4f31e65aa559",
+    "role_id": "62011052aadbcc1442b4b159",
+    "theme": 0
+}
+// export const dummyUser = undefined;  
