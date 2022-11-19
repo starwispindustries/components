d6e75cc782c5b098cc7c33e0efbcef71dd68652b
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-19 16:44:00 +0530

    fix: bugs

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,9 +1,9 @@
 // export const dummyUser = {
-//     "token": "7349abe3-c041-4118-a270-e88280197782",
+//     "token": "aff6685a-2268-4bb9-8e4b-4ca4c0760071",
 //     "username": "rj_a",
 //     "when_accepted_terms": 1668265016,
 //     "organization_id": "101",
-//     "session_id": "efbd58fb-aac6-4b75-833e-e0cef934f0d1",
+//     "session_id": "300b3835-d86b-44f0-92a9-9c7b0ec5cb36",
 //     "role_id": "62011052aadbcc1442b4b159",
 //     "theme": 0
 // }
