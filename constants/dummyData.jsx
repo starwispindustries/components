dae9783e54031b4ede810cb55a3a5ab16752dc04
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-26 19:11:20 +0530

    update: cmp

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -5,6 +5,6 @@
 //     "organization_id": "101",
 //     "session_id": "0898204f-5c2a-4355-aa32-e91b7f54e9d7",
 //     "role_id": "62011052aadbcc1442b4b159",
-//     "theme": 1
+//     "theme": 0
 // }
 export const dummyUser = undefined;  
