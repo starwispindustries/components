44ff01443e84f745eadd8b177e604cd0f3d55e8a
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-28 13:55:50 +0530

    fix: mark att

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,9 +1,9 @@
 // export const dummyUser = {
-//     "token": "75a4cf9c-812c-4720-b774-910210b89c1f",
+//     "token": "8473dc9f-26f3-4462-91a5-bf978504dc5c",
 //     "username": "null",
 //     "when_accepted_terms": 1668608395,
 //     "organization_id": "101",
-//     "session_id": "0898204f-5c2a-4355-aa32-e91b7f54e9d7",
+//     "session_id": "c1ec6d07-7ce0-4a22-84cf-d42e94c8dfeb",
 //     "role_id": "62011052aadbcc1442b4b159",
 //     "theme": 0
 // }
