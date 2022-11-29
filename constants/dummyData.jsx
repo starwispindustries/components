c210a51c087841ad5ba4e1dc02380807918a3705
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-29 23:18:16 +0530

    fix: 8474

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,10 +1,10 @@
 // export const dummyUser = {
-//     "token": "75a4cf9c-812c-4720-b774-910210b89c1f",
-//     "username": "null",
-//     "when_accepted_terms": 1668608395,
+//     "token": "64321a23-68fe-41ab-9926-c26c136b0f2a",
+//     "username": "nero2",
+//     "when_accepted_terms": 991562412,
 //     "organization_id": "101",
-//     "session_id": "0898204f-5c2a-4355-aa32-e91b7f54e9d7",
+//     "session_id": "69a70229-7572-4bbc-a025-04047eb5082a",
 //     "role_id": "62011052aadbcc1442b4b159",
-//     "theme": 0
+//     "theme": 2
 // }
 export const dummyUser = undefined;  
