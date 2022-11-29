03220107e5aa7ce09f8b18a074ae104f4e7f91f2
Ritik-Sharma38 <rd@ritiksharma.in>
2022-11-29 12:22:58 +0530

    fix: validate user

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,9 +1,9 @@
 // export const dummyUser = {
-//     "token": "a30c0a40-bae1-453b-8120-f6a3b134c19e",
-//     "username": "stress12",
-//     "when_accepted_terms": 1668443089,
+//     "token": "e1b0523a-d9b0-44b5-8217-01d47a1a2371",
+//     "username": "rj_a",
+//     "when_accepted_terms": 1668265016,
 //     "organization_id": "101",
-//     "session_id": "fe49b61c-433b-4412-a306-bb676871f9bc",
+//     "session_id": "ebd8b8ea-e421-472f-a272-f62dbd7d167b",
 //     "role_id": "62011052aadbcc1442b4b159",
 //     "theme": 0
 // }
