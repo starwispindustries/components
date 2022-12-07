9b4eee8423e3ff7845dc565d4cbeddc2bc88dc30
Ritik-Sharma38 <rd@ritiksharma.in>
2022-12-07 23:55:04 +0530

    added: whiteboard

--- constants/dummyData.jsx
+++ constants/dummyData.jsx
@@ -1,9 +1,9 @@
 // export const dummyUser = {
-//     "token": "3b557563-d72d-48fe-b7e2-45ace3ed40b8",
+//     "token": "41673aa6-5ed1-4cac-90de-e7a0a37dbe52",
 //     "username": "rj_c",
 //     "when_accepted_terms": 991562412,
 //     "organization_id": "101",
-//     "session_id": "ad67c334-a0d4-4d7b-8ce4-2c1f1080a049",
+//     "session_id": "2646594c-2c0d-4e8a-8cde-d251471f8654",
 //     "role_id": "62011052aadbcc1442b4b159",
 //     "theme": 0
 // }
